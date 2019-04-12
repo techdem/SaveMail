8648a6c43d297662080d710b14222d5e4e7639ec
techdem <tudor.chiribes@gmail.com>
2019-04-12 16:30:03 +0100

    added sender parsing

--- SaveMail/MainRibbon.cs
+++ SaveMail/MainRibbon.cs
@@ -18,16 +18,36 @@ namespace SaveMail
         private void Button1_Click(object sender, RibbonControlEventArgs e)
         {
             var browsePath = new FolderBrowserDialog();
+            String emailName = "";
 
             foreach(MailItem email in new Microsoft.Office.Interop.Outlook.Application().ActiveExplorer().Selection)
             {
-                if (email != null)
+                if(email != null)
                 {
+                    if(email.SenderEmailType == "EX")
+                    {
+                        AddressEntry address = email.Sender;
+
+                        if(address.AddressEntryUserType == OlAddressEntryUserType.olExchangeUserAddressEntry || address.AddressEntryUserType == OlAddressEntryUserType.olExchangeRemoteUserAddressEntry)
+                        {
+                            ExchangeUser internalAddress = address.GetExchangeUser();
+
+                            if(internalAddress != null)
+                            {
+                                emailName = internalAddress.PrimarySmtpAddress;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        emailName = email.SenderEmailAddress;
+                    }
+
                     DialogResult result = browsePath.ShowDialog();
 
-                    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(browsePath.SelectedPath))
+                    if(result == DialogResult.OK && !string.IsNullOrWhiteSpace(browsePath.SelectedPath))
                     {
-                        email.SaveAs(browsePath.SelectedPath+"test.msg", OlSaveAsType.olMSG);
+                        email.SaveAs(browsePath.SelectedPath+emailName+".msg", OlSaveAsType.olMSG);
                         MessageBox.Show("Item saved in: " + browsePath.SelectedPath);
                     }
                 }
