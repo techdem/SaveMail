ce16c256a84617493d97856deab9f07c763d879e
techdem <tudor.chiribes@gmail.com>
2019-04-24 12:34:26 +0100

    building up on tests

--- SaveMail/SaveMail.cs
+++ SaveMail/SaveMail.cs
@@ -10,8 +10,6 @@ namespace SaveMail
 {
     public class SaveMail
     {
-        static String emailName = "";
-
         public static object[] GetPath(FolderBrowserDialog fbd)
         {
             DialogResult dialogResult = fbd.ShowDialog();
@@ -20,34 +18,49 @@ namespace SaveMail
             return new object[] { dialogResult, selectedPath };
         }
 
+        public static String GetEmailOrigin(MailItem email)
+        {
+            String emailName = "";
+
+            if (email.SenderEmailType == "EX")
+            {
+                AddressEntry address = email.Sender;
+
+                if (address.AddressEntryUserType == OlAddressEntryUserType.olExchangeUserAddressEntry || address.AddressEntryUserType == OlAddressEntryUserType.olExchangeRemoteUserAddressEntry)
+                {
+                    ExchangeUser internalAddress = address.GetExchangeUser();
+
+                    if (internalAddress != null)
+                    {
+                        emailName = internalAddress.PrimarySmtpAddress;
+                    }
+                }
+            }
+            else
+            {
+                emailName = email.Sender.Address;
+            }
+
+            return emailName;
+        }
+
         public static bool SaveSelected(object[] savePath)
         {
+            String emailName;
+
             foreach (MailItem email in new Microsoft.Office.Interop.Outlook.Application().ActiveExplorer().Selection)
             {
                 if (email != null)
                 {
-                    if (email.SenderEmailType == "EX")
-                    {
-                        AddressEntry address = email.Sender;
-
-                        if (address.AddressEntryUserType == OlAddressEntryUserType.olExchangeUserAddressEntry || address.AddressEntryUserType == OlAddressEntryUserType.olExchangeRemoteUserAddressEntry)
-                        {
-                            ExchangeUser internalAddress = address.GetExchangeUser();
-
-                            if (internalAddress != null)
-                            {
-                                emailName = internalAddress.PrimarySmtpAddress;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        emailName = email.SenderEmailAddress;
-                    }
+                    emailName = GetEmailOrigin(email);
 
                     if ((DialogResult)savePath[0] == DialogResult.OK && !string.IsNullOrWhiteSpace((String)savePath[1]))
                     {
                         email.SaveAs(savePath[1] + email.ReceivedTime.ToString("dd-MM-yyyy") + " " + emailName + " " + email.Subject + ".msg", OlSaveAsType.olMSG);
+                    }
+                    else
+                    {
+                        return false;
                     }
                 }
             }
--- TDD/TestRibbon.cs
+++ TDD/TestRibbon.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Outlook;
+using System;
 using System.Diagnostics;
 using System.Windows.Forms;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -12,15 +13,30 @@ namespace UnitTests
         public void TestGetPath()
         {
             object[] testGetPath = SaveMail.SaveMail.GetPath(new FolderBrowserDialog());
+
             Assert.AreEqual(DialogResult.OK, testGetPath[0]);
             Assert.IsFalse(string.IsNullOrWhiteSpace((String)testGetPath[1]));
         }
 
         [TestMethod]
+        public void TestGetEmailOrigin()
+        {
+            Microsoft.Office.Interop.Outlook.Application outlookApplication = new Microsoft.Office.Interop.Outlook.Application();
+            Recipient outlookAddress = outlookApplication.Session.CreateRecipient("test@internal.address");
+            MailItem internalMailItem = (MailItem)outlookApplication.CreateItem(OlItemType.olMailItem);
+            internalMailItem.Sender = outlookAddress.AddressEntry;
+
+            Assert.AreEqual("test@internal.address", SaveMail.SaveMail.GetEmailOrigin(internalMailItem));
+        }
+
+        [TestMethod]
         public void TestSaveSelected()
         {
-            object[] testSaveSelected = new object[] { DialogResult.OK, "savePath" };
-            Assert.IsTrue(SaveMail.SaveMail.SaveSelected(testSaveSelected));
+            object[] mockOkResult = new object[] { DialogResult.OK, "savePath" };
+            object[] mockNegativeResult = new object[] { DialogResult.OK, " " };
+
+            Assert.IsTrue(SaveMail.SaveMail.SaveSelected(mockOkResult));
+            Assert.IsFalse(SaveMail.SaveMail.SaveSelected(mockNegativeResult));
         }
     }
 }
