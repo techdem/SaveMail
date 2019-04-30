734dc33401131392b1b68a65b8bc977d8ca11fd9
techdem <tudor.chiribes@gmail.com>
2019-04-30 16:30:16 +0100

    added UI class

---
+++ SaveMail/SaveMailUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveMail
+{
+    public class SaveMailUI
+    {
+        public object GetPath()
+        {
+            return new object();
+        }
+    }
+}
---
+++ TDD/TestUI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SaveMail;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestUI
+    {
+        [TestMethod]
+        public void SaveMailUI()
+        {
+            SaveMailUI saveMailUI = new SaveMailUI();
+            
+            Assert.IsInstanceOfType(saveMailUI.GetPath(), typeof(object));
+        }
+    }
+}
